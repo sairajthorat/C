46d6e9063c082cfc96b51f51ca7d7b253e86c345
Sairaj Thorat <thoratsairaj10@gmail.com>
2023-11-03 23:11:39 +0530

    c pracrice

---
+++ 4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int yr,lpy,ldays;
+    printf("Enter year");
+    if(yr%4==0){
+        lpy = yr/4;
+        ldays=lpy*366;
+        
+    }
+}
--- 6.c
+++ 6.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int fact =1 ,i=1,n;
+    long fact =1 ,i=1,n;
     printf("enter number to find factorial");
     scanf("%d",&n);
     while(i<=n)
